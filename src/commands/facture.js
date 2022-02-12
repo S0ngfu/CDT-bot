63822c338d01d79bb215795457bc2a57d8569785
S0ngfu <simon.gourdeau@hotmail.fr>
2022-02-12 22:45:19 +0100

    WIP facture command

--- src/commands/facture.js
+++ src/commands/facture.js
@@ -7,38 +7,81 @@ module.exports = {
 		.setName('facture')
 		.setDescription('Permet de faire une facture')
 		.setDefaultPermission(false)
-		.addStringOption((option) =>
-			option
-				.setName('client')
-				.setDescription('Permet de choisir l\'entreprise')
-				.setRequired(true)
-				.addChoice('ARC', '1')
-				.addChoice('Benny\'s', '2')
-				.addChoice('Blé d\'Or', '3')
-				.addChoice('Weazle News', '4')
-				.addChoice('Gouvernement', '5')
-				.addChoice('Mairie BC', '6')
-				.addChoice('Mairie LS', '7')
-				.addChoice('M$T', '8')
-				.addChoice('Paradise', '9')
-				.addChoice('Particulier', 'NULL')
-				.addChoice('PBSC', '10')
-				.addChoice('PLS', '11')
-				.addChoice('Rapid\'Transir', '12')
-				.addChoice('Rogers', '13')
-				.addChoice('SBC', '14')
-				.addChoice('Ryan\'s', '15'),
-		).addIntegerOption((option) =>
-			option
-				.setName('montant')
-				.setDescription('Montant de la facture')
-				.setRequired(true)
-				.setMinValue(1),
-		).addStringOption((option) =>
-			option
-				.setName('libelle')
-				.setDescription('Libellé de la facture')
-				.setRequired(true),
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('débit')
+				.setDescription('Permet d\'enregistrer un achat')
+				.addStringOption((option) =>
+					option
+						.setName('client')
+						.setDescription('Permet de choisir l\'entreprise')
+						.setRequired(true)
+						.addChoice('ARC', '1')
+						.addChoice('Benny\'s', '2')
+						.addChoice('Blé d\'Or', '3')
+						.addChoice('Weazle News', '4')
+						.addChoice('Gouvernement', '5')
+						.addChoice('Mairie BC', '6')
+						.addChoice('Mairie LS', '7')
+						.addChoice('M$T', '8')
+						.addChoice('Paradise', '9')
+						.addChoice('Particulier', 'NULL')
+						.addChoice('PBSC', '10')
+						.addChoice('PLS', '11')
+						.addChoice('Rapid\'Transit', '12')
+						.addChoice('Rogers', '13')
+						.addChoice('SBC', '14')
+						.addChoice('Ryan\'s', '15'),
+				).addIntegerOption((option) =>
+					option
+						.setName('montant')
+						.setDescription('Montant de la facture')
+						.setRequired(true)
+						.setMinValue(1),
+				).addStringOption((option) =>
+					option
+						.setName('libelle')
+						.setDescription('Libellé de la facture')
+						.setRequired(true),
+				),
+		)
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('crédit')
+				.setDescription('Permet d\'enregistrer une vente')
+				.addStringOption((option) =>
+					option
+						.setName('client')
+						.setDescription('Permet de choisir l\'entreprise')
+						.setRequired(true)
+						.addChoice('ARC', '1')
+						.addChoice('Benny\'s', '2')
+						.addChoice('Blé d\'Or', '3')
+						.addChoice('Weazle News', '4')
+						.addChoice('Gouvernement', '5')
+						.addChoice('Mairie BC', '6')
+						.addChoice('Mairie LS', '7')
+						.addChoice('M$T', '8')
+						.addChoice('Paradise', '9')
+						.addChoice('Particulier', 'NULL')
+						.addChoice('PBSC', '10')
+						.addChoice('PLS', '11')
+						.addChoice('Rapid\'Transit', '12')
+						.addChoice('Rogers', '13')
+						.addChoice('SBC', '14')
+						.addChoice('Ryan\'s', '15'),
+				).addIntegerOption((option) =>
+					option
+						.setName('montant')
+						.setDescription('Montant de la facture')
+						.setRequired(true)
+						.setMinValue(1),
+				).addStringOption((option) =>
+					option
+						.setName('libelle')
+						.setDescription('Libellé de la facture')
+						.setRequired(true),
+				),
 		),
 	async execute(interaction) {
 		const client = parseInt(interaction.options.getString('client')) || null;
@@ -46,6 +89,7 @@ module.exports = {
 		const libelle = interaction.options.getString('libelle');
 
 		console.log(client);
+		/*
 		await Bill.upsert({
 			date_bill: moment.tz('Europe/Paris'),
 			id_enterprise: client,
@@ -53,6 +97,7 @@ module.exports = {
 			sum_bill: montant,
 			info: libelle,
 		});
+		*/
 		return await interaction.reply({ content: 'Facture enregistré', ephemeral: true });
 	},
 };
