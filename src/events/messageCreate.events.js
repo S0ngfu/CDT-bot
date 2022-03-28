673e8baf2cd800d47518a500d1cf23f5e55702d8
Simon Gourdeau <simon.gourdeau@hotmail.fr>
2022-03-28 10:00:59 +0200

    Fix message fetched + regex match

--- src/events/messageCreate.events.js
+++ src/events/messageCreate.events.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: 'messageCreate',
 	async execute(message) {
-		if (!message.author.bot && message.channelId !== '731193069877067846') {
+		if (!message.webhookId) {
 			return;
 		}
 
@@ -10,7 +10,7 @@ module.exports = {
 
 		dmChannel.send({ content: message.content, embeds: message.embeds });
 
-		const embedsArray = JSON.stringify(message.embeds, undefined, 2).match(/(.{1,4000})/g);
+		const embedsArray = JSON.stringify(message.embeds, undefined, 2).match(/(.{1,4000})/gs);
 		console.log(embedsArray);
 		for (const e of embedsArray) {
 			dmChannel.send({ content: e });
