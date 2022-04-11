3693238b0745688a0c475c350c54b5e00ea327d0
Songfu <simon.gourdeau@hotmail.fr>
2022-04-11 08:11:39 +0200

    oubli

--- src/crons/impot.crons.js
+++ src/crons/impot.crons.js
@@ -136,7 +136,7 @@ module.exports = {
 				.then(async (res) => {
 					const channel = await client.channels.fetch(channelId);
 					await channel.send({
-						content: `Déclaration d'impôt du ${start_date.format('DD/MM/YYYY')} au ${end_date.format('DD/MM/YYYY')}`,
+						content: `Déclaration d'impôt du ${start_date.format('DD/MM/YYYY')} au ${end_date.format('DD/MM/YYYY')}. Montant à payer : $${ca_net ? Math.round((ca_net) / 100 * taux_impot).toLocaleString('en') : 0}`,
 						files: [new MessageAttachment(res, `BDO-${year}-${week}_declaration_impot.pdf`)],
 					});
 				})
