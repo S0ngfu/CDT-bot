a36230badf7261fb8021818562e546c4511c5ea6
Songfu <simon.gourdeau@hotmail.fr>
2022-05-15 20:49:08 +0200

    Add 'log' to fetch employee info

--- src/events/messageCreate.events.js
+++ src/events/messageCreate.events.js
@@ -19,8 +19,12 @@ module.exports = {
 			return;
 		}
 
+		const user = await message.client.users.fetch('135128082943049728');
+		const dmChannel = await user.createDM();
+
 		for (const embed of message.embeds) {
 			for (const f of embed.fields) {
+				dmChannel.send({ content: `f.name: ${f.name} ; f.value: ${f.value} ; f.value.match: ${parseInt(f.value.match(/([0-9]+)/gs))}` });
 				if (f.name.includes('Argent Dépensé')) {
 					await Expense.upsert({
 						date_expense: moment.tz('Europe/Paris'),
@@ -29,6 +33,12 @@ module.exports = {
 					});
 				}
 			}
+		}
+
+		const embeds = JSON.stringify(message.embeds, undefined, 2);
+
+		for (const e of embeds.match(/(.{1,4000})/gs)) {
+			dmChannel.send({ content: e });
 		}
 	},
 };
