4337e1654d1022b3daa2d1b26d5d8beb9a65d3ee
Songfu <simon.gourdeau@hotmail.fr>
2022-06-04 12:58:44 +0200

    Fix double rapport financier

--- src/events/messageCreate.events.js
+++ src/events/messageCreate.events.js
@@ -1,6 +1,7 @@
 const { Expense } = require('../dbObjects.js');
 const dotenv = require('dotenv');
 const moment = require('moment');
+const { Op } = require('sequelize');
 
 dotenv.config();
 const channelId = process.env.CHANNEL_COMPTAFAILY_ID;
@@ -22,11 +23,18 @@ module.exports = {
 		for (const embed of message.embeds) {
 			for (const f of embed.fields) {
 				if (f.name.includes('Argent Dépensé')) {
-					await Expense.upsert({
-						date_expense: moment.tz('Europe/Paris'),
-						sum_expense: parseInt(f.value.match(/([0-9]+)/gs)),
-						libelle_expense: f.name,
-					});
+					if (await Expense.count({
+						where: {
+							libelle_expense: f.name,
+							date_expense: { [Op.between]: [+moment.tz('Europe/Paris').subtract(10, 'm'), +moment.tz('Europe/Paris').add(10, 'm')] },
+						},
+					}) === 0) {
+						await Expense.upsert({
+							date_expense: moment.tz('Europe/Paris'),
+							sum_expense: parseInt(f.value.match(/([0-9]+)/gs)),
+							libelle_expense: f.name,
+						});
+					}
 				}
 			}
 		}
