8b93a24794cf1c4b474f49587bcf32e004c429fa
Songfu <simon.gourdeau@hotmail.fr>
2022-06-25 13:49:52 +0200

    Add autocomplete for model name

--- src/events/interactionCreate.events.js
+++ src/events/interactionCreate.events.js
@@ -1,5 +1,5 @@
 const { Modal, TextInputComponent, MessageActionRow, MessageEmbed } = require('discord.js');
-const { Enterprise, Product, Group, Employee } = require('../dbObjects');
+const { Enterprise, Product, Group, Employee, BillModel } = require('../dbObjects');
 const { Op } = require('sequelize');
 const dotenv = require('dotenv');
 
@@ -42,6 +42,11 @@ module.exports = {
 					}
 					const employees = await Employee.findAll({ attributes: ['name_employee'], order: [['name_employee', 'ASC']], where: where, limit: 25 });
 					const choices = employees.map(e => ({ name: e.name_employee, value: e.name_employee }));
+					await interaction.respond(choices);
+				}
+				else if (focusedOption.name === 'nom_modèle') {
+					const bill_models = await BillModel.findAll({ attributes: ['name'], order: [['name', 'ASC']], where: { id_employe: interaction.user.id, name: { [Op.like]: `%${focusedOption.value}%` } }, limit: 25 });
+					const choices = bill_models.map(bm => ({ name: bm.name, value: bm.name }));
 					await interaction.respond(choices);
 				}
 			}
