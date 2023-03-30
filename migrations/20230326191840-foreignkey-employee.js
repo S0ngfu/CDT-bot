bec0f8f40a4c7f3ecbee781fa1085f03cf56486f
Songfu <simon.gourdeau@hotmail.fr>
2023-03-30 11:00:07 +0200

    Fix missing employees

--- migrations/20230326191840-foreignkey-employee.js
+++ migrations/20230326191840-foreignkey-employee.js
@@ -10,9 +10,142 @@ moment.updateLocale('fr', {
 	},
 });
 
+const employeesToCreate = [
+	{
+		name_employee: '172608328130756609',
+		id_employee: '172608328130756609',
+		date_hiring: moment('2022-03-28 00:00:00'),
+		date_firing: moment('2022-05-10 23:59:59'),
+	},
+	{
+		name_employee: '197016574928879616',
+		id_employee: '197016574928879616',
+		date_hiring: moment('2022-03-29 00:00:00'),
+		date_firing: moment('2022-04-03 23:59:59'),
+	},
+	{
+		name_employee: '204749079618519041',
+		id_employee: '204749079618519041',
+		date_hiring: moment('2022-04-12 00:00:00'),
+		date_firing: moment('2022-05-14 23:59:59'),
+	},
+	{
+		name_employee: '235419823117565952',
+		id_employee: '235419823117565952',
+		date_hiring: moment('2022-04-15 00:00:00'),
+		date_firing: moment('2022-04-22 23:59:59'),
+	},
+	{
+		name_employee: 'James Alburn',
+		id_employee: '245279138909388800',
+		date_hiring: moment('2022-04-15 00:00:00'),
+		date_firing: moment('2022-04-26 23:59:59'),
+	},
+	{
+		name_employee: 'Kjell Hopp',
+		id_employee: '260482377153773568',
+		date_hiring: moment('2022-01-11 00:00:00'),
+		date_firing: moment('2022-05-18 23:59:59'),
+	},
+	{
+		name_employee: 'Allen Harcours',
+		id_employee: '311920738212773888',
+		date_hiring: moment('2022-03-26 00:00:00'),
+		date_firing: moment('2022-05-22 23:59:59'),
+	},
+	{
+		name_employee: '427922783755042819',
+		id_employee: '427922783755042819',
+		date_hiring: moment('2022-03-21 00:00:00'),
+		date_firing: moment('2022-04-19 23:59:59'),
+	},
+	{
+		name_employee: '447367747521871873',
+		id_employee: '447367747521871873',
+		date_hiring: moment('2022-03-25 00:00:00'),
+		date_firing: moment('2022-05-06 23:59:59'),
+	},
+	{
+		name_employee: 'Margarett Ills',
+		id_employee: '534029005381304322',
+		date_hiring: moment('2022-03-22 00:00:00'),
+		date_firing: moment('2022-04-12 23:59:59'),
+	},
+	{
+		name_employee: '622015200119488542',
+		id_employee: '622015200119488542',
+		date_hiring: moment('2022-03-31 00:00:00'),
+		date_firing: moment('2022-04-03 23:59:59'),
+	},
+	{
+		name_employee: '937393336787009596',
+		id_employee: '937393336787009596',
+		date_hiring: moment('2022-03-27 00:00:00'),
+		date_firing: moment('2022-04-07 23:59:59'),
+	},
+];
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
 	async up(queryInterface, Sequelize) {
+
+		// Create missing employees
+		for (const e of employeesToCreate) {
+			await Employee.create(e);
+		}
+
+		// Fix problems with existing employees
+		await Employee.update(
+			{
+				date_hiring: moment('2022-11-25 14:00:00'),
+			},
+			{
+				where: {
+					id_employee: '279746970510295041',
+				},
+			},
+		);
+		await Employee.update(
+			{
+				date_hiring: moment('2022-05-12 00:00:00'),
+			},
+			{
+				where: {
+					id_employee: '334345556296073226',
+				},
+			},
+		);
+		await Employee.update(
+			{
+				date_hiring: moment('2022-05-12 12:00:00'),
+			},
+			{
+				where: {
+					id_employee: '440089030374457347',
+				},
+			},
+		);
+		await Employee.update(
+			{
+				date_hiring: moment('2022-11-16 12:00:00'),
+			},
+			{
+				where: {
+					id_employee: '531017285318475776',
+				},
+			},
+		);
+		await Employee.update(
+			{
+				date_hiring: moment('2022-03-25 12:00:00'),
+			},
+			{
+				where: {
+					id_employee: '646447778096087071',
+				},
+			},
+		);
+
 		const employees = await Employee.findAll();
 
 		for (const employee of employees) {
