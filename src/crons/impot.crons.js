e267844f1595dbb420d8672d017b2bdbf7b4e156
Songfu <simon.gourdeau@hotmail.fr>
2023-05-29 11:54:54 +0200

    Fix EPIPE

--- src/crons/impot.crons.js
+++ src/crons/impot.crons.js
@@ -6,7 +6,6 @@ const dotenv = require('dotenv');
 const moment = require('moment');
 const pdf = require('pdf-creator-node');
 const fs = require('fs');
-const phantomjs = require('phantomjs-prebuilt');
 
 dotenv.config();
 const channelId = process.env.CHANNEL_COMPTA_ID;
@@ -136,7 +135,6 @@ module.exports = {
 				// 'buffer' or 'stream' or ''
 			};
 			const options_pdf = {
-				phantomPath: phantomjs.path,
 				format: 'A4',
 				orientation: 'portrait',
 				border: '10mm',
